ef44d7a4cc9cd054cee04f9fc2d43a461bc79ae2
Vadim <duf1lt@yandex.ru>
2022-09-01 16:55:35 +0300

    Initialize project

---
+++ Lab1/app/src/main/java/narkevich/fit/bstu/by/MainActivity.kt
@@ -0,0 +1,11 @@
+package narkevich.fit.bstu.by
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
