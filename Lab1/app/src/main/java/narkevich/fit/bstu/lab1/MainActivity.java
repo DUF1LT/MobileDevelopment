6a7cd581dd724ad79dbc5357031e05ae0f69501c
Vadim <duf1lt@yandex.ru>
2022-09-03 09:56:54 +0300

    Complete lab1

--- Lab1/app/src/main/java/narkevich/fit/bstu/lab1/MainActivity.java
+++ Lab1/app/src/main/java/narkevich/fit/bstu/lab1/MainActivity.java
@@ -3,6 +3,10 @@ package narkevich.fit.bstu.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import narkevich.fit.bstu.lab1.text.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", "counter = " + count);
+        }
+
+        displayText();
+    }
+
+    private void displayText() {
+        var textFunction = new TextFunction();
+
+        TextView nt = findViewById(R.id.main_view);
+        nt.setText(textFunction.getValue());
     }
 }
---
+++ Lab1/app/src/main/java/narkevich/fit/bstu/lab1/text/TextFunction.java
@@ -0,0 +1,7 @@
+package narkevich.fit.bstu.lab1.text;
+
+public class TextFunction {
+    public String getValue() {
+        return "Hello from first project";
+    }
+}
